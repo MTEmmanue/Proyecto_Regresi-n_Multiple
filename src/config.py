fd9d61d80cbeb98230a79e8ac06b90f19de72a09
MTEmmanue <emmanuel.tp.2024@gmail.com>
2025-11-12 22:10:25 -0600

    Iniciando pipeline

---
+++ src/config.py
@@ -0,0 +1,8 @@
+# src/config.py
+
+# Ruta a tu dataset
+DATA_PATH = r"C:\Users\Emmanuel\Documents\Proyecto_Regresión_Multiple\Data\CO2 Emissions_Canada.csv"
+
+
+# Ruta donde se guardará el modelo entrenado
+MODEL_PATH = "models/co2_model.joblib"
---
+++ src/data_loader.py
@@ -0,0 +1,11 @@
+# src/data_loader.py
+
+import pandas as pd
+from config import DATA_PATH  # <-- Importa la ruta desde tu config
+
+def load_data():
+    """
+    Carga los datos desde la ruta especificada en config.py.
+    """
+    print(f"Cargando datos desde: {DATA_PATH}")
+    return pd.read_csv(DATA_PATH)
